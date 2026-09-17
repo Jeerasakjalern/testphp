2e26db4022365fda3bc87ea8120e1bd039f29997
Jeerasakjalern <jeerasak.jalernpon@gmail.com>
2026-09-17 13:55:40 +0700

    Function

---
+++ 1-testft01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Writing PHP Function</title>
+</head>
+<body>
+    <h2>first time for php</h2>
+    <?php
+
+        /*Defining a PHP function*/
+        function writeMessage() {
+            echo "You are really a nice person, Have a nice time!";
+        }
+        /*calling a php function*/
+        writeMessage();
+    ?>
+</body>
+</html>
